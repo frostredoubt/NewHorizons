540a2f85ff5559bad766455735ca375c5f24c033
Fil Krynicki <filipkrynicki@gmail.com>
2015-09-25 22:52:32 -0700

    Server-owned cubes are movin and groovin

---
+++ Assets/FilTestNetworkShipMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class FilTestNetworkShipMovement : NetworkBehaviour
+{
+
+    public Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isServer)
+        {
+            transform.Translate(velocity);
+        }
+	}
+}
