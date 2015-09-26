c9531a0b9c403114170f5d0d68f17f619f22cc22
Alex <Alex>
2015-09-26 15:13:24 -0400

    Alex initial push of collision detection and movement

---
+++ Assets/Game/firing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class firing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(100, 100, 100) };
+        Vector2[] uvs = new Vector2[mesh.vertices.Length];
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(mesh.vertices[i].x, mesh.vertices[i].z);
+        }
+        mesh.uv = uvs;
+    }
+}
