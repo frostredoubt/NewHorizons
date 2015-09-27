c874dc5606471f79ebe40cdb7504010e03bbdf0a
Ben Gaciu <bengaciu@Bens-MacBook-Pro.local>
2015-09-27 13:43:09 -0400

    adding health slider for ships

---
+++ Assets/SliderToNumber.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderToNumber : MonoBehaviour {
+
+	public Text text;
+
+	private Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider> ();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (slider && text) {
+			text.text = slider.value.ToString() + "%";
+		}
+	}
+}
