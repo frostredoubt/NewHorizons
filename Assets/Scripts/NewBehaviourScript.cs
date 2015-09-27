07a0f6d386aa93f0807d790d734c8d16dc9f333b
Tyler <stszepes@uwaterloo.ca>
2015-09-27 08:13:08 -0400

    added a little scene with particle effects that get triggered by script

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public ParticleSystem ps;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space")) { 
+            if (!ps.IsAlive()) ps.Play();
+        }
+	}
+}
