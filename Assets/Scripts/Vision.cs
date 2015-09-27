c58b2cc16e120bb5eebae20c283edbc87bba9179
Alex <Alex>
2015-09-27 13:22:44 -0400

    Correct comparison for owner ship of vision bubble

--- Assets/Scripts/Vision.cs
+++ Assets/Scripts/Vision.cs
@@ -65,7 +65,7 @@ public class Vision : MonoBehaviour {
     void OnMouseEnter()
     {
         Ship s = gameObject.transform.parent.GetComponent("Ship") as Ship;
-        if (gameObject.name == "Model" && s.player )
+        if (gameObject.name == "Model" && s.player == Game.singleton.local_player )
         {
             Vision_object.GetComponent<MeshRenderer>().enabled = true;
         }
@@ -74,7 +74,7 @@ public class Vision : MonoBehaviour {
     void OnMouseExit()
     {
         Ship s = gameObject.transform.parent.GetComponent("Ship") as Ship;
-        if ( gameObject.name == "Model" && s.player )
+        if ( gameObject.name == "Model" && s.player == Game.singleton.local_player )
         {
             Vision_object.GetComponent<MeshRenderer>().enabled = false;
         }
