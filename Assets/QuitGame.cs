ed0c47c1176c43b6fa88f28582e1b5db3820d4b9
Ben Gaciu <bengaciu@Bens-MacBook-Pro.local>
2015-09-27 23:18:07 -0400

    trying to see if anyone else is doing anything

--- Assets/QuitGame.cs
+++ Assets/QuitGame.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class QuitGame : MonoBehaviour {
 
 	public void Exit() {
-		UnityEditor.EditorApplication.isPlaying = false; //makes the editor stop playing game too
 		Application.Quit ();
 	}
 }
